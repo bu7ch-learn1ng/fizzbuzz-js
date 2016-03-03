6c70809e03b2553911fdafd58db04306aa9c8592
celiathurel <celiathurel@gmail.com>
2016-03-03 12:16:49 +0100

    second

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,19 +2,19 @@
 for (var i = 1 ; i <= 100 ; i ++) {
   var i = Number(prompt("Entrez un nombre"));
   if ((i % 3 === 0) && (i % 5 !== 0)) {
-    console.log("fizz");
+    return("fizz");
     break;
 
   } else if ((i % 5 === 0) && (i % 3 !== 0)) {
-    console.log("buzz");
+    return("buzz");
     break;
 
   } else if ((i % 5 === 0) && (i % 3 === 0)) {
-    console.log("fizzbuzz");
+    return("fizzbuzz");
     break;
 
   } else {
-  console.log(i);
+  return(i);
   break;
   }
 }
